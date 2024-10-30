d3efe45fe043d468de21da0d2f446497d1037018
venkataPhanindraVutla <phanivutla2004@gmail.com>
2024-10-30 12:29:30 +0530

    All questions

--- base/views.py
+++ base/views.py
@@ -8,3 +8,22 @@ def login(request):
     return render(request, "login.html")
 def register(request):
     return render(request,'register.html')
+
+def all_questions(request):
+    dic =[
+        {
+            "title": "How to build a StackOverflow clone?",
+            "question": "I want to build a StackOverflow clone. Where can I get some help?",
+            "author": "Santhi",
+            "date": "1m ago",
+            "score": 1,
+        },
+        {
+            "title": "How to import pandas?",
+            "question": "Please help me import pandas. Where can I get some help?",
+            "author": "Santhi",
+            "date": "1m ago",
+            "score": 1,
+        }
+        ]
+    return render(request,'all_questions.html',{'questions':dic})
